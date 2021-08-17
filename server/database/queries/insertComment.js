b8ee147fe0cae93995d7abf55dae761336d320df
Abdallah <dev.aaamra@gmail.com>
2021-08-17 22:09:34 +0300

    fix values

--- server/database/queries/insertComment.js
+++ server/database/queries/insertComment.js
@@ -3,6 +3,6 @@ const connection = require('../config/connection');
 const insertComment = ({
   username, body, postId,
 }) => connection
-  .query('insert into comments (username, body, post_id) returning id, body', [username, body, postId]);
+  .query('insert into comments (username, body, post_id) values($1,$2,$3) returning id, body', [username, body, postId]);
 
 module.exports = insertComment;
