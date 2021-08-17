6f52d327d4303a860f192c798093b01c3f5bc447
Nizar7zak <nizar7zakout@gmail.com>
2021-08-17 23:02:20 +0300

    handle endpoints relates #12

--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { createPost, showPosts } = require('./postController');
+const { createPost, showPosts, showSinglePost } = require('./postController');
+const { showComments, createComment } = require('./commentsController');
 
 const router = express.Router();
 
 router.get('/posts', showPosts);
+router.get('/posts/:postId', showSinglePost);
+router.get('/comments/:postId', showComments);
 router.post('/post/create', createPost);
+router.post('/comment/create/:postId', createComment);
 
 module.exports = router;
--- server/database/queries/insertPost.js
+++ server/database/queries/insertPost.js
@@ -3,6 +3,6 @@ const connection = require('../config/connection');
 const insertPost = ({
   username, title, body, createdAt,
 }) => connection
-  .query('insert into posts (username,title, body, created_at) values($1,$2,$3,$4) returning id, title', [username, title, body, createdAt]);
+  .query('insert into posts (username, title, body, created_at) values($1, $2, $3, $4)', [username, title, body, createdAt]);
 
 module.exports = insertPost;
