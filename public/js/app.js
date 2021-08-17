1e7f0cd30d3920cc45328df840ba39908ee32d9c
Abdallah <dev.aaamra@gmail.com>
2021-08-17 17:16:07 +0300

    file structure and design

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+const modalButton = document.querySelector('#modal-button');
+const modal = document.querySelector('#modal');
+const closeButton = document.querySelector('.close');
+
+modalButton.addEventListener('click',() => {
+    modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+})
