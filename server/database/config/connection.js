0bad2979c696f6d03903a5b9538873a27298675d
Nizar7zak <nizar7zakout@gmail.com>
2021-08-18 13:00:57 +0300

    handle cases for url test, dev and production Relates #16

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,8 +1,18 @@
 require('env2')('.env');
 const { Pool } = require('pg');
 
-const { DB_URL } = process.env;
-if (!DB_URL) throw new Error('sth wroing');
+const { DB_URL, TEST_DB_URL, DEV_DB_URL } = process.env;
+
+let dbUrl = '';
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = TEST_DB_URL;
+} else if (process.env.NODE_ENV === 'development') {
+  dbUrl = DEV_DB_URL;
+} else {
+  dbUrl = DB_URL;
+}
+if (!dbUrl) throw new Error('sth wroing');
+
 const options = {
   connectionString: DB_URL,
   ssl: false,
