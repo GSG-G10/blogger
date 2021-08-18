e18e3d2a1b5696e832e6a540e457808d459c3836
Abdallah <dev.aaamra@gmail.com>
2021-08-18 10:28:55 +0300

    make insert post transaction

--- server/database/queries/insertCategoryPost.js
+++ server/database/queries/insertCategoryPost.js
@@ -1,6 +1,5 @@
-const connection = require('../config/connection');
+// const connection = require('../config/connection');
 
-const insertCategoryPost = ({ categoryId, postId }) => connection
-  .query('insert into category_post (category_id, post_id) values($1,$2)', [categoryId, postId]);
 
-module.exports = insertCategoryPost;
+
+// module.exports = insertCategoryPost;
--- server/database/queries/insertPost.js
+++ server/database/queries/insertPost.js
@@ -1,8 +1,27 @@
 const connection = require('../config/connection');
 
-const insertPost = ({
-  username, title, body, createdAt,
-}) => connection
-  .query('insert into posts (username,title, body, created_at) values($1,$2,$3,$4) returning id, title', [username, title, body, createdAt]);
+const addPost = async ({
+  username, title, body, createdAt, categories,
+}) => {
+  try {
+    await connection.query('BEGIN');
 
-module.exports = insertPost;
+    const insertPost = await connection
+      .query('insert into posts (username,title, body, created_at) values($1,$2,$3,$4) returning id, title', [username, title, body, createdAt]);
+
+    await insertPost().then((data) => {
+      const postId = data.rows[0].id;
+      categories.forEach((id) => {
+        connection
+          .query('insert into category_post (category_id, post_id) values($1,$2)', [id, postId]);
+      });
+    });
+
+    await connection.query('COMMIT');
+  } catch (e) {
+    await connection.query('ROLLBACK');
+    throw e;
+  }
+};
+
+module.exports = addPost;
