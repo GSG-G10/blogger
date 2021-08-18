0670c67b06120ea43bb43a5362502317106a911d
Abdallah <dev.aaamra@gmail.com>
2021-08-18 12:06:18 +0300

    improve insert post

--- server/database/queries/insertPost.js
+++ server/database/queries/insertPost.js
@@ -1,34 +1,7 @@
 const connection = require('../config/connection');
 
-<<<<<<< HEAD
-const addPost = async ({
-  username, title, body, createdAt, categories,
-}) => {
-  try {
-    await connection.query('BEGIN');
-=======
 const insertPost = ({
-  username, title, body, createdAt,
-}) => connection
-  .query('insert into posts (username, title, body, created_at) values($1, $2, $3, $4) RETURNING id, title', [username, title, body, createdAt]);
->>>>>>> ba40c2a8e1e3e62ed6c890bccf56b3a50091eafd
+  username, title, body, createdAt, categoryId,
+}) => connection.query('insert into posts (username,title, body, created_at,category_id) values($1,$2,$3,$4,$5) returning id, title', [username, title, body, createdAt, categoryId]);
 
-    const insertPost = await connection
-      .query('insert into posts (username,title, body, created_at) values($1,$2,$3,$4) returning id, title', [username, title, body, createdAt]);
-
-    await insertPost().then((data) => {
-      const postId = data.rows[0].id;
-      categories.forEach((id) => {
-        connection
-          .query('insert into category_post (category_id, post_id) values($1,$2)', [id, postId]);
-      });
-    });
-
-    await connection.query('COMMIT');
-  } catch (e) {
-    await connection.query('ROLLBACK');
-    throw e;
-  }
-};
-
-module.exports = addPost;
+module.exports = insertPost;
